35dbd8d6f05cdd6502ce5aad59cfda5e7173e226
juno5626 <junono1@naver.com>
2026-03-07 14:28:39 +0900

    1week assignment setting

--- 1주차/C001.c
+++ 1주차/C001.c
@@ -2,39 +2,32 @@
 
 int main(){
 
-    //test github
     int size; // 크기
     int stars, blank1, blank2; // 별의 갯수, 빈칸1의 갯수, 빈칸2의 갯수 
     int i, j; // 반복문을 위한 변수
-/*
+
     printf("크기를 입력하시오: ");
     scanf("%d", &size);
     for(j = 1 ; j<= 4 ; j++){
         if(j==1){
             for(i =1 ; i<=size ;i++){
                 for(int k =1; k<=size*2;k++){
-                    if(k<=size*2-i+1 || k==i){
+                    if(k==size*2-i+1 || k==i){
                         printf("*");
                     }else{
                         printf(" ");
                     }
+                    if(k==size*2){
+                        printf("\n");
+                    }
                 }
             }
+        }else if(j==2){
+            for()
         }
+            
         
     }
-*/
-    for(int j =0;j<5;j++){
-        
-        for(i=0;i<10;i++){
-            if(i==0||i==9){
-                printf("*");
-            }else{
-                printf(" ");
-            }
-            if(i==9){
-                printf("\n");
-            }
-        }
-    }
+
+    
 }
